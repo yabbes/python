e944a3c2b1d11009074cf472ab9a3527f7f99d05
yabbes <yabbaspost@gmail.com>
2017-02-08 22:22:47 +0100

    bissl neues

--- bot.py
+++ bot.py
@@ -23,29 +23,35 @@ class Botzentrum:
     def sendmsg(self, msg):
         irc.send(bytes("PRIVMSG "+ self.channel +" :"+ msg +"\n", "UTF-8"))
     def quit(self):
+        self.sendmsg("Arrividerci")
         irc.send(bytes('DISCONNECT \n', "UTF-8"))
         exit(0)
 
 
 def main():
-    bot = Botzentrum('irc.freenode.net', '##francophonie')
+    bot = Botzentrum('irc.freenode.net', '##tobbowobbo')
     bot.connect()
 
     while True:
        text=irc.recv(2040).decode("UTF-8")  #receive the text
        text=text.strip('\n\r')
-       print(text)   #print text to console
-
+       print(text)
        if text.find("PRIVMSG") != -1:
            name = text.split('!',1)[0][1:]
            message = text.split('PRIVMSG',1)[1].split(':',1)[1]
-           if message.find(bot.nick) != -1:
+           if message.find("Salut " + bot.nick) != -1:
                bot.sendmsg("Salut " + name + " !!")
+           if message.find('tu ' + bot.nick) != -1:
+               bot.sendmsg('Moi, quoi ?')
+           if message.find('partir ' + bot.nick) != -1:
+               bot.sendmsg('dac je pars')
+               bot.quit()
+
            #bot.sendmsg('Hallo')
 
-       #command = input()
-       #if command == '.q':
-           #bot.quit()
+    #    command = input()
+    #    if command == '.q':
+    #        bot.quit()
 
 
 if __name__ == '__main__':
