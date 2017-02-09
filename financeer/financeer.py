eb5b73b3837a82f57cd371c7dabe04746f2f4144
yabbes <yabbaspost@gmail.com>
2017-02-09 21:52:18 +0100

    help functions

--- financeer/financeer.py
+++ financeer/financeer.py
@@ -6,12 +6,44 @@ class Financeer:
     def __init__(self, addedAmount):
         self.b = BankDatabase()
         self.accountBalance = self.b.getAccountBalance()
+        self.temp = addedAmount
         self.newAccountBalance = self.b.getAccountBalance() + addedAmount
         self.accountName = self.b.getAccountName()
         self.transactionCount = self.b.getNumberOfTransactions()
     def mainLoop(self):
-        print('Aktueller Kontostand: \t {0} \t aus: {1}'.format(self.accountBalance,
-        self.accountName))
+        userInput = input()
+        if userInput.startswith('.help'):
+            self.helpFunctions()
+        if userInput.startswith('.name'):
+            self.name()
+        if userInput.startswith('.temp'):
+            self.temp()
+        if userInput.startswith('.balance'):
+            self.balance()
+        if userInput.startswith('.count'):
+            self.count()
+        if userInput.startswith('.quit'):
+            self.quit()
+
+
+    def helpFunctions(self):
+        print('Financeer kennt folgende Funktionen: \n'
+        '.balance \t show account balance\n'
+        '.temp \t\t show _temporary_ money\n'
+        '.name \t\t show names of connected bank accounts\n'
+        '.count \t\t show transaction count for bank accounts\n'
+        '.quit \t\t quit Financeer(r) \n')
+    def name(self):
+        print('Currently analysing account: {0}\n'.format(self.accountName))
+    def balance(self):
+        print('Current bank account balance {0}\n'.format(self.accountBalance))
+    def temp(self):
+        print('Currently handling {0} € of temporary monies \n'.format(self.temp))
+        print('if you were to save it to your bank account, this would make {0}'.format(self.newAccountBalance))
+    def count(self):
+        print('Currently counting {0} transactions on this bank account \n'.format(self.transactionCount))
+    def quit(self):
+        print('Ciao\n')
         exit(0)
 
 
@@ -25,7 +57,8 @@ if __name__ == '__main__':
         'Benutze >>0€<<\n')
         monies = 0
     fin = Financeer(monies)
-
+    print('Aktueller Kontostand: \t {0} \t aus: {1}'.format(fin.accountBalance,
+    fin.accountName))
     while True:
         fin.mainLoop()
 
