a5f55ea91d9694d789900c9dbc32139d45b83a75
yabbes <yabbaspost@gmail.com>
2017-02-09 23:12:12 +0100

    bug fix for changing properties

--- financeer/financeer.py
+++ financeer/financeer.py
@@ -5,24 +5,27 @@ from db import BankDatabase
 class Financeer:
     def __init__(self, addedAmount):
         self.b = BankDatabase()
+        self.userInput = ''
         self.accountBalance = self.b.getAccountBalance()
         self.temp = addedAmount
-        self.newAccountBalance = self.b.getAccountBalance() + addedAmount
+        self.newAccountBalance = self.b.getAccountBalance() + self.temp
         self.accountName = self.b.getAccountName()
         self.transactionCount = self.b.getNumberOfTransactions()
     def mainLoop(self):
-        userInput = input()
-        if userInput.startswith('.help'):
+        self.userInput = input()
+        if self.userInput.startswith('.help'):
             self.helpFunctions()
-        if userInput.startswith('.name'):
+        if self.userInput.startswith('.name'):
             self.name()
-        if userInput.startswith('.temp'):
+        if self.userInput.startswith('.temp'):
             self.temporary()
-        if userInput.startswith('.balance'):
+        if self.userInput.startswith('.balance'):
             self.balance()
-        if userInput.startswith('.count'):
+        if self.userInput.startswith('.count'):
             self.count()
-        if userInput.startswith('.quit'):
+        if self.userInput.startswith('.add'):
+            self.add()
+        if self.userInput.startswith('.quit'):
             self.quit()
 
 
@@ -32,14 +35,29 @@ class Financeer:
         '.temp \t\t show _temporary_ money\n'
         '.name \t\t show names of connected bank accounts\n'
         '.count \t\t show transaction count for bank accounts\n'
+        '.add \t\t add money to _temporary money_\n'
         '.quit \t\t quit Financeer(r) \n')
     def name(self):
         print('Currently analysing account: {0}\n'.format(self.accountName))
+    def getNewAccountBalance(self):
+        return self.newAccountBalance
+    def add(self):
+        amt_str = self.userInput.split(' ',1)[1]
+        try:
+            amt_int = int(amt_str)
+            self.temp = self.temp + amt_int
+            self.newAccountBalance = self.newAccountBalance + amt_int
+            print('Now handling {0} of temporary monies'.format(self.getTemporary()))
+            # return self.temp + self.newAccountBalance
+        except ValueError:
+            print('The second parameter of .add is supposed to be an int')
     def balance(self):
         print('Current bank account balance {0}\n'.format(self.accountBalance))
+    def getTemporary(self):
+        return self.temp
     def temporary(self):
-        print('Currently handling {0} € of temporary monies \n'.format(self.temp))
-        print('if you were to save it to your bank account, this would make {0}'.format(self.newAccountBalance))
+        print('Currently handling {0} € of temporary monies \n'.format(self.getTemporary()))
+        print('if you were to save it to your bank account, this would make {0}'.format(self.getNewAccountBalance()))
     def count(self):
         print('Currently counting {0} transactions on this bank account \n'.format(self.transactionCount))
     def quit(self):
