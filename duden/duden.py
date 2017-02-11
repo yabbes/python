40ab5d964b56149905d9ca5eee190138952e272a
yabbes <yabbaspost@gmail.com>
2017-02-11 16:30:47 +0100

    cleaned code, abandoned crawling description text for the moment and removed debugging code snippets

--- duden/duden.py
+++ duden/duden.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # Duden word lookup and spelling checker
+# coding=utf-8
 
 import urllib.request
 from bs4 import BeautifulSoup
@@ -18,23 +19,24 @@ def main(word):
 
 def parse(data):
     soup = BeautifulSoup(data, 'lxml')
-
     firstResult_filter = {"id":"block-duden-tiles-0"}
-    noResult_filter = {"id":"block-system-main"}
-
     firstResult = soup.find('section', firstResult_filter)
+    # for later tries, getting description text as well maybe
+    # sections = firstResult.findAll('section', {"class":"wide"})
+    # for item in sections:
+    #     print('{0}'.format(item.next_sibling()))
+
     firstResult_headers = firstResult.findAll('a', {"class":"hidden-link"})
 
     for item in firstResult_headers:
         print('{0}'.format(item.text))
 
-    f = open("duden.html", 'w')
-    soup_str = str(firstResult_headers)
-    f.write(soup_str)
-    f.close()
+    # debugging purposes:
+    # f = open("duden.html", 'w')
+    # soup_str = str(firstResult_headers)
+    # f.write(soup_str)
+    # f.close()
 
-    # noResult = soup.find('section', noResult_filter)
-    # print('{0}\n {1}\n'.format(firstResult_headers.text(), noResult))
 
 
 if __name__ == '__main__':
@@ -43,8 +45,10 @@ if __name__ == '__main__':
         try:
             main(word)
         except Exception as e:
-            print('This word either does not exist or is not spelled correctly\n'
-            '{}'.format(e))
+            # because duden.de does not handle httprequests for words
+            # that don't exist, we can simply conclude the following
+            # at least for our purposes it will be just fine
+            print('This word either does not exist or is not spelled correctly\n')
     else:
         print("Please enter the word for lookup as parameter\n"
         "Correct syntax : ./duden.py ##word##\n")
